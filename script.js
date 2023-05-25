585bee401dc9136f7d6c3a1c8372de9f4e266de7
SHUBHAM SHAH <subhamsah73@gmail.com>
2023-05-25 11:47:21 +0545

    SMS frontend Completed

---
+++ script.js
@@ -0,0 +1,43 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  window.onscroll = function() {
+    var scrollToTopButton = document.getElementById('scrollToTopButton');
+    if (window.pageYOffset > 200) {
+      scrollToTopButton.style.display = 'block';
+    } else {
+      scrollToTopButton.style.display = 'none';
+    }
+  };
+
+  const sliderElm = document.querySelector(".slider-container .slider");
+  const btnLeft = document.querySelector(".slider-container .btn-left");
+  const btnRight = document.querySelector(".slider-container .btn-right");
+
+  const numberSliderBoxs = sliderElm.children.length;
+  let idxCurrentSlide = 0;
+
+  // Functions:
+  function moveSlider() {
+      let leftMargin = (sliderElm.clientWidth / numberSliderBoxs) * idxCurrentSlide;
+      sliderElm.style.marginLeft = -leftMargin + "px";
+      console.log(sliderElm.clientWidth, leftMargin);
+  }
+  function moveLeft() {
+      if (idxCurrentSlide === 0) idxCurrentSlide = numberSliderBoxs - 1;
+      else idxCurrentSlide--;
+
+      moveSlider();
+  }
+  function moveRight() {
+      if (idxCurrentSlide === numberSliderBoxs - 1) idxCurrentSlide = 0;
+      else idxCurrentSlide++;
+
+      moveSlider();
+  }
+
+  // Event Listeners:
+  btnLeft.addEventListener("click", moveLeft);
+  btnRight.addEventListener("click", moveRight);
+  window.addEventListener("resize", moveSlider);
